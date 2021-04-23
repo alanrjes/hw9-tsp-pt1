b5076723c300fce3acd17e8f5015095a52a69c11
Alan <alanrjes@gmail.com>
2021-04-22 21:49:39 -0700

    Filled in some methods
    Compiles, not run-tested yet

--- cities.cc
+++ cities.cc
@@ -1,25 +1,59 @@
 #include "cities.hh"
+#include <cmath>
 
-Cities::Cities() {  // constructor
-  //
+Cities::Cities(std::vector<coord_t> map) {  // constructor
+  map_ = map;
 }
 
-istream& operator>> (std::istream&, Cities&) {
-  //
+std::istream& Cities::operator>> (std::istream& is) {
+  int len = map_.size();
+  for (int i=0; i<len; i++) {
+    coord_t coords = map_[i];
+    is>> std::get<0>(coords)>> std::get<1>(coords);
+  }
+  return is;
 }
 
-ostream& operator<< (std::ostream&, Cities&) {
-  //
+std::ostream& Cities::operator<< (std::ostream& os) {
+  int len = map_.size();
+  for (int i=0; i<len; i++) {
+    coord_t coords = map_[i];
+    os<< std::get<0>(coords)<< std::get<1>(coords);
+  }
+  return os;
 }
 
-double total_path_distance(const permutation_t& ordering) const {
-  //
+double Cities::total_path_distance(const permutation_t& ordering) const {
+  int len = ordering.size();
+  coord_t firstcoords = map_[ordering[0]];
+  coord_t prevcoords;
+  double dist;  // sum of distance
+
+  for (int i=1; i<=len; i++) {
+    coord_t coords;
+    if (i == len) {  // if it's the last iteration
+      coords = firstcoords;  // find returning distance to the first city
+    }
+    else {
+      coords = map_[ordering[i]];  // coords of next city in permutation, adjusted by ordering index
+    }
+    double subdist = std::sqrt(std::pow((std::get<0>(coords) - std::get<0>(prevcoords)), 2) + std::pow((std::get<1>(coords) - std::get<1>(prevcoords)), 2));
+    dist += subdist;
+    prevcoords = coords;
+  }
+  return dist;
 }
 
-Cities reorder(const permutation_t& ordering) const {
-  //
+Cities Cities::reorder(const permutation_t& ordering) const {
+  int len = ordering.size();
+  std::vector<coord_t> nmap(len);
+  for (int i=0; i<len; i++) {
+    coord_t coords = nmap[ordering[i]];
+    nmap[i] = coords;
+  }
+  return Cities(nmap);
 }
 
-permutation_t random_permutation(unsigned len) {
+permutation_t Cities::random_permutation(unsigned len) {
   //
 }
--- cities.hh
+++ cities.hh
@@ -2,27 +2,27 @@
 
 #include <iostream>
 #include <vector>
+#include <utility>
 
 // Representation of an ordering of cities
 class Cities {
-private:
-  istream& operator>> (std::istream&, Cities&);
-  ostream& operator<< (std::ostream&, Cities&);
-
 public:
   // A pair of integral coordinates for each city
   using coord_t = std::pair<int, int>;
-
   // An ordering of cities. Each value represents a unique index into the current city ordering in some container.
   using permutation_t = std::vector<unsigned int>;
 
+  Cities(std::vector<coord_t> map);
+
   // Given a permutation, return a new Cities object with the city coords indexed in the order given by the permutation.
   Cities reorder(const permutation_t& ordering) const;
-
   // Given a permutation in a Cities object, compute the distance through all the cities in the given order, returning to the first city.
-  // The distance between any two cities is computed as the Euclidean distance on a plane between their coordinates.
   double total_path_distance(const permutation_t& ordering) const;
-
   // Generates and returns a new permutation of all the numbers from 0 to len-1 (with no skipped or repeated indices).
   permutation_t random_permutation(unsigned len);
+
+private:
+  std::istream& operator>> (std::istream& is);
+  std::ostream& operator<< (std::ostream& os);
+  std::vector<coord_t> map_;
 };
